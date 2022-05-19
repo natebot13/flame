ec7841696c43293a4fc4b325621ebcb84c02a752
Nathan Pannell <tekkbot@gmail.com>
2022-05-18 22:50:39 -0700

    added trimmed sprite support

--- packages/flame/lib/src/sprite.dart
+++ packages/flame/lib/src/sprite.dart
@@ -20,13 +20,23 @@ class Sprite {
   Image image;
   Rect src = Rect.zero;
 
+  // represents the original rectangle of the sprite in a trimmed spritesheet.
+  // For example: if sprite is originally 16x16, but trimmed down to 8x8, we can
+  // capture the original size and offset into the 16x16 rect to reproduce the
+  // correct positioning of this sprite in 16x16.
+  Rect untrimmedSrc = Rect.zero;
+
   Sprite(
     this.image, {
     Vector2? srcPosition,
     Vector2? srcSize,
+    Vector2? untrimmedPosition,
+    Vector2? untrimmedSize,
   }) {
     this.srcSize = srcSize;
     this.srcPosition = srcPosition;
+    this.untrimmedPosition = untrimmedPosition;
+    this.untrimmedSize = untrimmedSize;
   }
 
   /// Takes a path of an image, a [srcPosition] and [srcSize] and loads the
@@ -60,6 +70,18 @@ class Sprite {
 
   set srcPosition(Vector2? position) {
     src = (position ?? Vector2.zero()).toPositionedRect(srcSize);
+  }
+
+  Vector2 get untrimmedSize => Vector2(untrimmedSrc.width, untrimmedSrc.height);
+
+  set untrimmedSize(Vector2? size) {
+    untrimmedSrc = untrimmedPosition.toPositionedRect(size ?? srcSize);
+  }
+
+  Vector2 get untrimmedPosition => untrimmedSrc.topLeft.toVector2();
+
+  set untrimmedPosition(Vector2? position) {
+    untrimmedSrc = (position ?? Vector2.zero()).toPositionedRect(untrimmedSize);
   }
 
   /// Same as [render], but takes both the position and the size as a single
@@ -111,8 +133,10 @@ class Sprite {
     _tmpRenderSize.setFrom(size ?? srcSize);
 
     _tmpRenderPosition.setValues(
-      _tmpRenderPosition.x - (anchor.x * _tmpRenderSize.x),
-      _tmpRenderPosition.y - (anchor.y * _tmpRenderSize.y),
+      _tmpRenderPosition.x -
+          ((anchor.x * untrimmedSize.x) - untrimmedPosition.x),
+      _tmpRenderPosition.y -
+          ((anchor.y * untrimmedSize.y) - untrimmedPosition.y),
     );
 
     final drawRect = _tmpRenderPosition.toPositionedRect(_tmpRenderSize);
